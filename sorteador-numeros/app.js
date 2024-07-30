751c916aee3517ed02f576c02063e3b1df7b910b
Vagner Guimarães <euvagnersg@gmail.com>
2024-07-30 15:05:08 -0300

    Sorteio de numero aleatorio

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -3,11 +3,18 @@ function sortear(){
     let de = parseInt(document.getElementById("de").value);
     let ate = parseInt(document.getElementById("ate").value);
 
-    let numero = obternumeroaleatorio(de, ate);
-    console.log(numero);
+    let sorteados = [];
+    let numero;
+
+    for (let i = 0; i < quantidade; i++){
+        numero = obterNumeroAleatorio(de, ate);
+        sorteados.push(numero);
+    }    
+    
+    alert(sorteados);
 }
 
-function obternumeroaleatorio(min, max){
+function obterNumeroAleatorio(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
